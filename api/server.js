dbc28a2ddeae2502f348b9308ae8f79c4de524bd
Ness Reliford <nessreliford@gmail.com>
2024-12-06 15:31:05 -0800

    GET users

--- api/server.js
+++ api/server.js
@@ -1,9 +1,20 @@
 
 const express = require('express')
 const server = express()
+const User = require('./users/model')
 
-
-
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "error getting users",
+            err: err.message
+        })
+    })
+})
 
 
 
