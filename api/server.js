f8abd9675c8fe905228f5ad48c9043bd6a7e5971
Ness Reliford <nessreliford@gmail.com>
2024-12-06 15:43:00 -0800

    GET user by ID

--- api/server.js
+++ api/server.js
@@ -16,6 +16,23 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user){
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        }
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "error getting users",
+            err: err.message
+        })
+    })
+})
 
 
 server.use("*", (req, res) => {
