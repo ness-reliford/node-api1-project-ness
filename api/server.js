646ee681ee003113845c2ddc3eba2a4fe04b4547
Ness Reliford <nessreliford@gmail.com>
2024-12-07 08:43:15 -0800

    DELETE user

--- api/server.js
+++ api/server.js
@@ -58,6 +58,26 @@ server.post('/api/users', (req, res) => {
     
 })
 
+server.delete('/api/users/:id', async (req, res) => {
+    const possibleUser = await User.findById(req.params.id)
+    if(!possibleUser){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist",
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+})
+
+
+// server.put('/api/users/:id', (req, res) => {
+//     const user = req.body
+//     User.update(user)
+//     .then()
+//     .catch()
+// })
+
 server.use("*", (req, res) => {
     res.status(404).json({
         message: "not found"
